2effb11ae8e5d9512d7696ecbb3201ad4bac79fc
jingangdong <286659206@qq.com>
2018-04-28 10:17:54 +0800

    修改请求

--- plugins/fetch.js
+++ plugins/fetch.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const fetch = (url, data = {}, token = '') => {
-  const baseUrl = 'http://localhost:3000'
-  // const baseUrl = 'http://115.159.104.224'
+  // const baseUrl = 'http://localhost:3000'
+  const baseUrl = 'http://115.159.104.224'
   return new Promise((resolve, reject) => {
     axios({
       method: 'post',
